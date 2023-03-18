b39326ab07bc485d44a269df8294d357a2442d12
schalk <sv@easybooking.at>
2023-03-18 08:12:03 +0100

    start 3.6

---
+++ cmd/api/movies.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/schalkwv/greenlight/internal/data"
+	"net/http"
+	"time"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
+}
